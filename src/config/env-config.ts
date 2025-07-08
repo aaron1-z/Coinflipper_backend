4ab47371df6bb46dc2e9adc69013ac0f03d832ba
aaron1-z <adityakittu2773@gmail.com>
2025-07-08 16:48:54 +0530

    modified env config

--- src/config/env-config.ts
+++ src/config/env-config.ts
@@ -8,10 +8,10 @@ export const config = {
      amqpExchangeName: process.env.AMQP_EXCHANGE_NAME || '',
 
      db: {
-        host: process.env.DB_HOST || '127.0.0.1',
-        user: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || 'Aaron12@',
-        database: process.env.DB_NAME || 'coin_flipper_db',
+        host: process.env.DB_HOST || '',
+        user: process.env.DB_USER || '',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_NAME || '',
         port: Number(process.env.DB_PORT) || 3306,
      }
 };
